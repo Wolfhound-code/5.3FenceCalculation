c2db892ee524a790b0a0e4dec9ccd504c0b447b8
Dmitry <dimromashev@mail.ru>
2025-08-20 23:21:06 +0300

    Play random

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,47 @@
 package org.example
 
 import kotlin.math.roundToInt
+import kotlin.random.Random
+import kotlin.text.compareTo
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("Введите длину первой стороны")
-    val a = readLine()?.toDouble() ?: 0.0
-    println("Введите длину второй стороны")
-    val b = readLine()?.toDouble() ?: 0.0
-    println("Введите длину третьей стороны")
-    val c = readLine()?.toDouble() ?: 0.0
-    println("Введите длину четвертой стороны")
-    val d = readLine()?.toDouble() ?: 0.0
-    calcFenceMaterials(a, b, c, d)
-}
-fun calcFenceMaterials(a: Double, b: Double, c: Double, d: Double) {
-    val minSide = 2
-    val maxSide = 300
-    val polesGap = 2.5 // расстояние между столбами
-    val listWidth = 1.15 // ширина листа
-    if (a < minSide || b < minSide || c < minSide || d < minSide) {
-        println("Стороны участка должны быть не менее $minSide метров")
-        return
+    val hiddenNumber = Random.nextInt(20) + 1
+    var attempt = 1
+    println("Загадано число от 1 до 20. Попробуйте его угадать")
+    do {
+        // вводим данные
+        val userNumber = getUserNumber()
+        when {
+            userNumber == hiddenNumber -> {
+                println("Вы угадали, было загадано число: $hiddenNumber! Количество попыток: $attempt")
+                return
+            }
+
+            hiddenNumber > userNumber -> {
+                println("Загаданное число больше")
+                println("Попытка №: $attempt")
+            }
+
+            else -> {
+                println("Загаданное число меньше")
+                println("Попытка №: $attempt")
+            }
+        }
+        attempt++
+    } while (true)
     }
-    if (a > maxSide || b > maxSide || c > maxSide || d > maxSide) {
-        println("Стороны участка должны быть не более $maxSide метров")
-        return
-    }
-    val perimeter = a + b + c + d
-    val polesAmount = ((perimeter / polesGap) * 1.1).roundToInt()
-    val fenceListAmount = ((perimeter / listWidth) * 1.15).roundToInt()
-    println("Необходимое количество столбов: $polesAmount")
-    println("Необходимое количество листов профлиста: $fenceListAmount")
+
+    fun getUserNumber(): Int {
+    do {
+        println("Введите число от 1 до 20")
+        val number = readLine()!!.toInt()
+        if (number < 1 || number > 20) {
+            println("Некорректный ввод!")
+        } else {
+            return number
+        }
+    } while (true)
+
 }
